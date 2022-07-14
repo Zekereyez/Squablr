f438cdc02e527f66efaa5d30659e0b7785ecace9
Zeke Reyes <zekereyes@fb.com>
2022-07-14 16:54:47 -0700

    Progress commit

---
+++ Squablr/Models/ProfilePictureCell.h
@@ -0,0 +1,17 @@
+//
+//  ProfilePictureCell.h
+//  Squablr
+//
+//  Created by Zeke Reyes on 7/14/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfilePictureCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Squablr/Models/ProfilePictureCell.m
@@ -0,0 +1,12 @@
+//
+//  ProfilePictureCell.m
+//  Squablr
+//
+//  Created by Zeke Reyes on 7/14/22.
+//
+
+#import "ProfilePictureCell.h"
+
+@implementation ProfilePictureCell
+
+@end
