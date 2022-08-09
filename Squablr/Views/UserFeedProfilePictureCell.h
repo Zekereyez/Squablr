0d64d66c78790a67aa9f2939c979ccf40cb9ec1d
Zeke Reyes <zekereyes@fb.com>
2022-08-09 14:34:18 -0700

    Added files for user profile pictures for feed

---
+++ Squablr/Views/UserFeedProfilePictureCell.h
@@ -0,0 +1,16 @@
+//
+//  UserFeedProfilePictureCell.h
+//  Squablr
+//
+//  Created by Zeke Reyes on 8/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserFeedProfilePictureCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Squablr/Views/UserFeedProfilePictureCell.m
@@ -0,0 +1,12 @@
+//
+//  UserFeedProfilePictureCell.m
+//  Squablr
+//
+//  Created by Zeke Reyes on 8/9/22.
+//
+
+#import "UserFeedProfilePictureCell.h"
+
+@implementation UserFeedProfilePictureCell
+
+@end
