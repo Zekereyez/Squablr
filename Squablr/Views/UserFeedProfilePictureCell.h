4962ed2ba3fb116a324687f6cc8dc6ef5867d70b
Zeke Reyes <zekereyes@fb.com>
2022-08-09 14:35:59 -0700

    Added PFImageView property to allow user profile images

--- Squablr/Views/UserFeedProfilePictureCell.h
+++ Squablr/Views/UserFeedProfilePictureCell.h
@@ -6,10 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UserFeedProfilePictureCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet PFImageView *matchedUserProfileImage;
 
 @end
 
